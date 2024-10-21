34f865d99daf88b0afd0edaa1698095200e377d9
Alex Schenk <134595144+alx-sch@users.noreply.github.com>
2024-10-21 12:41:54 +0000

    solution megaphone.cpp

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -6,15 +6,25 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:43:27 by aschenk           #+#    #+#             */
-/*   Updated: 2024/10/16 12:44:47 by aschenk          ###   ########.fr       */
+/*   Updated: 2024/10/21 12:40:21 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#define SHOUT(x) std::cout << (char)toupper(x);
 
-int	main(void)
+int main(int argc, char **argv)
 {
-	std::cout << "Hello, world!" << std::endl;
-
-	return (0);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			std::string str(av[i]);
+			for (size_t j = 0; j < str.length(); j++)
+				LOG(str[j]);
+		}
+		std::cout << std::endl;
+    }
+    return (0);
 }
